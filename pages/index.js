750bdf0e4b0a2a458b70c50804e0335e0ccdb830
sanchitpasricha <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>
2023-04-27 18:53:15 +0530

    setting up google authentication

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,24 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return (
-    <div className="bg-blue-900 w-screen h-screen flex items-center">
-      <div className="text-center w-full">
-        <button className="bg-white text-black px-4 p-2 rounded-lg">
-          Login with Google
-        </button>
+  const { data: session } = useSession();
+  if (!session) {
+    return (
+      <div className="bg-blue-900 w-screen h-screen flex items-center">
+        <div className="text-center w-full">
+          <button
+            onClick={() => signIn("google")}
+            className="bg-white text-black px-4 p-2 rounded-lg"
+          >
+            Login with Google
+          </button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+  return <div>logged in {session.user.email}</div>;
 }
