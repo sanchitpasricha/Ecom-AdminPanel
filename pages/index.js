97433cf8f0ab5f451897ee428ca6716917e893cd
sanchitpasricha <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>
2023-04-30 22:31:48 +0530

    importing session data to dashboard page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,21 @@
+/* eslint-disable @next/next/no-img-element */
 import Layout from "@/components/Layout";
+import { useSession } from "next-auth/react";
 
 export default function Home() {
-  return <Layout>Dashboard page here</Layout>;
+  const { data: session } = useSession();
+  return (
+    <Layout>
+      <div className="text-blue-900 flex justify-between">
+        <h2>
+          {" "}
+          Hello, <b>{session?.user?.name}</b>
+        </h2>
+        <div className="flex bg-gray-300 text-black gap-2 rounded-lg overflow-hidden">
+          <img src={session?.user?.image} className="w-8 h-8" alt=" " />
+          <span className="px-2">{session?.user?.name}</span>
+        </div>
+      </div>
+    </Layout>
+  );
 }
